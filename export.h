8834d5321bcdeb399f67d2020fd031c6d61d4139
Will Cassella <cassew@google.com>
2022-01-20 14:43:52 -0800

    Add note

--- export.h
+++ export.h
@@ -1,7 +1,9 @@
 #if defined(_MSC_VER)
-#define IMPORT __declspec(dllimport)
 #define EXPORT __declspec(dllexport)
+#define IMPORT __declspec(dllimport)
 #else
-#define IMPORT
 #define EXPORT __attribute__((visibility("default")))
+// Many projects define `IMPORT` as nothing here, causing the issue.
+// You can safely define it the same way as `EXPORT` to fix this problem.
+#define IMPORT
 #endif
